a7b575b97b12be2285e8c04ab96cbef92a3fba24
Tanya Singh <tanyacs27@gmail.com>
2020-06-05 17:37:33 +0530

    Contigous Array

--- Challenges/Contiguous Array.js
+++ Challenges/Contiguous Array.js
@@ -16,3 +16,24 @@ Note: The length of the given binary array will not exceed 50,000.
 */
 
 // ---------------------- IMPLEMENTATION --------------------------------
+var findMaxLength = function(nums) {
+    // Creating a map to store sum and index
+    let map = new Map();
+    // Setting initial sum and index in map
+    map.set(0, -1);  
+    // Created variable which will return the max length of sub-array
+    let maxLen = 0;
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        // Adding to -1 when 0 found else 1
+        sum += (nums[i] == 0) ? -1 : 1;
+        // If map has the sum already then compare it with current maxLen, else set it in map
+        if (map.has(sum)) {
+            maxLen = Math.max(maxLen, i - map.get(sum));
+        }
+        else {
+            map.set(sum, i);
+        }
+    }  
+    return maxLen;
+};
